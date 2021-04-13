7a8c4a3383eb4ce2cca072c997832e733e89fbfb
JPB <jbanas9124@gmail.com>
2021-04-13 14:29:36 -0500

    fs functions moved to generate-site.js distriution folder created to hold dynamically created html and css files

--- app.js
+++ app.js
@@ -76,7 +76,10 @@
 
 const inquirer = require('inquirer');
 
-const fs = require('fs');
+// no longer necessary as we are using generate-site.js for fs functions
+// const fs = require('fs');
+
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template');
 
 // const pageHTML = generatePage(names, github);
@@ -210,12 +213,18 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    
-
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-    });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
-
